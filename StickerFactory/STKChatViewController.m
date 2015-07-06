262b094dde2189c704b9dbb14c1e4757989631ce
Vadim Degterev <vadimdegterev@Vadims-MacBook-Pro.local>
2015-07-06 16:54:57 +0300

    Implement example chat

--- StickerFactory/STKChatViewController.m
+++ StickerFactory/STKChatViewController.m
@@ -9,12 +9,16 @@
 #import "STKChatViewController.h"
 #import "STKChatCell.h"
 
-@interface STKChatViewController() <UITableViewDelegate, UITableViewDataSource>
+@interface STKChatViewController() <UITableViewDelegate, UITableViewDataSource, UITextViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UITextView *inputTextView;
 
+@property (assign, nonatomic) BOOL isKeyboardShowed;
+
+
 @property (strong, nonatomic) NSArray *dataSource;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *textViewHeightConstraint;
 
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomViewConstraint;
 
@@ -27,19 +31,91 @@
     
     self.dataSource = @[@"[[pinkgorilla_bigsmile]]",@"[[pinkgorilla_china]]",@"[[pinkgorilla_bigsmile]]",@"[[pinkgorilla_bigsmile]]",@"[[pinkgorilla_bike]]",@"[[pinkgorilla_bigsmile]]",@"[[pinkgorilla_bigsmile]]",@"[[pinkgorilla_bigsmile]]",@"[[pinkgorilla_bigsmile]]",@"[[pinkgorilla_bigsmile]]",@"[[pinkgorilla_bigsmile]]",@"[[pinkgorilla_bigsmile]]",@"[[pinkgorilla_bigsmile]]",@"[[pinkgorilla_bigsmile]]",@"[[pinkgorilla_dontknow]]"];
     
-    [UIView setAnimationsEnabled:NO];
+    self.inputTextView.layer.cornerRadius = 7.0;
+    self.inputTextView.layer.borderWidth = 1.0;
+    self.inputTextView.layer.borderColor = [UIColor colorWithRed:0.84 green:0.84 blue:0.85 alpha:1].CGColor;
+    
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(willHideKeyboard:)
                                                  name:UIKeyboardWillHideNotification
                                                object:nil];
     
-    self.inputTextView.keyboardDismissMode = UIScrollViewKeyboardDismissModeInteractive;
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(didShowKeyboard:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillChangeFrame:) name:UIKeyboardWillChangeFrameNotification
+                                               object:nil];
+    
 }
 
 
+#pragma mark - Notifications
 
-- (void)willHideKeyboard:(NSNotification *)notification {
-    [UIView setAnimationsEnabled:NO];
+- (void) keyboardWillChangeFrame:(NSNotification*)notification {
+    //TODO: Change stickers panel size
+}
+
+- (void) didShowKeyboard:(NSNotification*)notification {
+    
+    CGRect keyboardBounds = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    
+    UIViewAnimationCurve curve = [[notification.userInfo objectForKey:UIKeyboardAnimationCurveUserInfoKey] integerValue];
+    CGFloat keyboardHeight = keyboardBounds.size.height;
+    
+    CGFloat animationDuration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] floatValue];
+    
+    self.bottomViewConstraint.constant = keyboardHeight;
+
+    
+    [UIView animateWithDuration:animationDuration animations:^{
+        [UIView setAnimationCurve:curve];
+        [self.view layoutIfNeeded];
+    }];
+    
+    self.isKeyboardShowed = YES;
+    
+}
+
+
+- (void)willHideKeyboard:(NSNotification*)notification {
+    
+    self.isKeyboardShowed = NO;
+    
+    CGFloat animationDuration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] floatValue];
+    
+    self.bottomViewConstraint.constant = 0;
+    
+    [UIView animateWithDuration:animationDuration animations:^{
+        [self.view layoutIfNeeded];
+    }];
+}
+
+- (IBAction)changeKeyboadViewAction:(UIButton*)button {
+    
+    UIImage *buttonImage = nil;
+    
+    if (self.inputTextView.inputView) {
+        buttonImage = [UIImage imageNamed:@"ShowKeyboadIcon"];
+        self.inputTextView.inputView = nil;
+        
+    } else {
+        buttonImage = [UIImage imageNamed:@"ShowStickersIcon"];
+        UIView *stickersView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 225)];
+        stickersView.backgroundColor = [UIColor redColor];
+        self.inputTextView.inputView = stickersView;
+    }
+    if (!self.isKeyboardShowed) {
+        [self.inputTextView becomeFirstResponder];
+    }
+    [self.inputTextView reloadInputViews];
+    [button setImage:buttonImage forState:UIControlStateNormal];
+    [button setImage:buttonImage forState:UIControlStateHighlighted];
+    
+
+    
 }
 
 
@@ -61,15 +137,12 @@
     return cell;
 }
 
-#pragma mark - UI
+#pragma mark - UITextViewDelegate
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    UIView *greenView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
-    greenView.backgroundColor = [UIColor greenColor];
-    
-    self.inputTextView.inputView = greenView;
-    
-    [self.inputTextView reloadInputViews];
+- (void)textViewDidChange:(UITextView *)textView  {
+    self.textViewHeightConstraint.constant = textView.contentSize.height;
 }
+
+
 
 @end
